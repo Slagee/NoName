612d5a86b2c3c493abd28e98e50ddfa8a27958cf
bergerino <panzenowski@gmail.com>
2021-12-10 03:51:21 +0100

    Uztofungujeee

--- Code/client/src/App.js
+++ Code/client/src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import "antd/dist/antd.css";
+import { Col, Layout, Row, Button, Table, Space, Input} from 'antd';
+import companyLogo from '../src/SDlogo.png';
+import { data, columns } from './Data.js';
+const { Header, Content } = Layout;
+const { Search } = Input;
+const onSearch = value => console.log(value);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Layout>
+        <Space direction= "vertical">
+        <Header style={{background:'whitesmoke', color:'rgba(0, 147, 221, 1)', boxShadow: '0px 2px 5px gray'}}>
+          <Row>
+            <Col span={0.5}><img src={companyLogo} alt="BigCo Inc. logo" width='40'/></Col>
+            <Col span={3}><p style={{fontSize: 20}}>Dokumentační systém</p></Col>
+            <Col span={19}></Col>
+            <Col span={1.5}><Button>Přihlásit se</Button></Col>
+          </Row>
+        </Header>
+
+        <Content style={{ padding: '25px 100px' }}>
+          <Search placeholder="Vyhledej záznam" onSearch={onSearch} enterButton style={{ width: 300, float: 'right', paddingBottom: '25px' }}/>
+          <Table
+            columns={columns}
+            dataSource={data}
+            title={() => 'Seznam profilů'}
+          />
+        </Content>
+        </Space>
+      </Layout>
     </div>
   );
 }
---
+++ Code/client/src/Data.js
@@ -0,0 +1,51 @@
+import "antd/dist/antd.css";
+import { Col, Layout, Row, Button, Table, Space, Tag} from 'antd';
+
+const columns = [
+    { title: 'Jméno', dataIndex: 'name', key: 'name', width: 300 },
+    { title: 'Datum narození', dataIndex: 'dob', key: 'dob', align: 'center' },
+    { title: 'Středisko', dataIndex: 'department', key: 'department', align: 'center' },
+    {
+      title: '',
+      dataIndex: '',
+      key: 'x',
+      render: () => <a>Zobrazit</a>,
+      align: 'center'
+    },
+    
+  ];
+  
+  const data = [
+    {
+      key: 1,
+      name: 'Adam Novák',
+      dob: '11.08.1974',
+      department: '100-RÚT Karviná'
+    },
+    {
+        key: 2,
+        name: 'František Dobrák',
+        dob: '08.02.1979',
+        department: '1033-CHANA Bruntál'
+    },
+    {
+        key: 3,
+        name: 'Veronika Kupcová',
+        dob: '28.06.1984',
+        department: '1071-EFFATHA Krnov'
+    },
+    {
+        key: 4,
+        name: 'Ondřej Tlustý',
+        dob: '01.04.1980',
+        department: '2062-KARMEL Tichá'
+    },
+    {
+      key: 5,
+      name: 'Jana Novotná',
+      dob: '22.10.1990',
+      department: '1017-BENJAMÍN Krnov'
+  },
+  ];
+
+  export { data, columns };
