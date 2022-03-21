bd407238a11ce74339b333fcddae3b8897d2640a
Tommy-Vlasy <thomas.v18@seznam.cz>
2022-03-21 19:26:42 +0100

    update list-of-centers

--- Code/client/src/components/listOfCenters/ListOfCenters.jsx
+++ Code/client/src/components/listOfCenters/ListOfCenters.jsx
@@ -2,81 +2,85 @@ import { Space, Table} from 'antd';
 import { Form, Input, Button, Row, Col, Modal } from 'antd';
 import './ListOfCenters.css';
 import { useState } from 'react';
-import { ArrowLeftOutlined, DeleteOutlined, PlusCircleOutlined } from "@ant-design/icons/lib/icons";
-
+import { ArrowLeftOutlined, DeleteOutlined, PlusCircleOutlined, EditOutlined} from "@ant-design/icons/lib/icons";
 const { Search } = Input;
 
 export default function ListOfCenters() {
 
 const [searchCenters, setSearchCenters] = useState(null)
 const [page, setPage] = useState(1)
+const [isEditing, setIsEditing] = useState(false);
+const [editingCenters, setEditingCenters] = useState(null);
 
-const [isModalVisible, setIsModalVisible] = useState(false);
-
-const showModal = () => {
-  setIsModalVisible(true);
-};
-
-const handleOk = () => {
-  setIsModalVisible(false);
-};
-
-const handleCancel = () => {
-  setIsModalVisible(false);
-};
-
+const [dataSource, setDataSource] = useState([
+  {
+    center: 'ÚSTŘEDÍ Český Těšín', 
+  },
+  {
+    center: 'BETHEL Karviná', 
+  },
+  {
+    center: 'BETANIA Komorní Lhotka', 
+  },
+  {
+    center: 'BETEZDA Komorní Lhotka', 
+  },
+  {
+    center: 'EBEN-EZER Český Těšín', 
+  }
+]);
 const columns = [
     {
+      key: 'center',
       title: 'Středisko',
       dataIndex: 'center',
-      key: 'center',
     },
     {
-      dataIndex: 'editing',
-      key: 'editing',
-      align: 'center',
-      width: '6%'
-    },
-    {
-      dataIndex: 'deletion',
-      key: 'deletion',
-      align: 'center',
-      width: '6%'
+      key: "actions",
+      render: (record) => {
+        return (
+        <>
+        <Button type="primary" className='EditButton' htmlType="submit" icon={<EditOutlined/>} onClick={() => {onEditCenter(record);}} style= {{marginLeft: 200 }}>Upravit</Button>
+        <Button className='DeleteButton' danger icon={<DeleteOutlined/>} onClick={() => {onDeleteCenter(record);}}>Odstranit</Button>
+          </>
+        );
+      },
     },
   ];
-  
-  const data = [
-    {
-      key: '1',
-      center: 'ÚSTŘEDÍ Český Těšín',
-      editing: <Button type="primary" htmlType="submit">Upravit</Button>,
-      deletion: <Button danger icon={<DeleteOutlined/>}>Odstranit</Button>, 
-    },
-    {
-      key: '2',
-      center: 'BETHEL Karviná',
-      editing: <Button type="primary" htmlType="submit">Upravit</Button>,
-      deletion: <Button danger icon={<DeleteOutlined/>}>Odstranit</Button>, 
-    },
-    {
-      key: '3',
-      center: 'BETANIA Komorní Lhotka',
-      editing: <Button type="primary" htmlType="submit">Upravit</Button>,
-      deletion: <Button danger icon={<DeleteOutlined/>}>Odstranit</Button>, 
-    },
-    {
-      key: '4',
-      center: 'BETEZDA Komorní Lhotka',
-      editing: <Button type="primary" htmlType="submit">Upravit</Button>,
-      deletion: <Button danger icon={<DeleteOutlined/>}>Odstranit</Button>, 
-    },
-    {
-      key: '5',
-      center: 'EBEN-EZER Český Těšín',
-      editing: <Button type="primary" htmlType="submit">Upravit</Button>,
-      deletion: <Button danger icon={<DeleteOutlined/>}>Odstranit</Button>, 
-    }
-  ];
+
+  const onAddCenter = () => {
+    const newCenter = {
+      center: "Středisko"
+    };
+    setDataSource((pre) => {
+      return [...pre, newCenter];
+    });
+  };
+
+  const onDeleteCenter = (record) => {
+    Modal.confirm({
+      title: "Opravdu chcete středisko smazat?",
+      okText: "ANO",
+      okType: "danger",
+      cancelText: "NE",
+      onOk: () => {
+        setDataSource((pre) => {
+          return pre.filter((center) =>(center.id !== center.id));
+        });
+      },
+    });
+  };
+
+  const onEditCenter = (record) => {
+    setIsEditing(true);
+    setEditingCenters({ ...record });
+  };
+
+  const resetEditing = () => {
+    setIsEditing(false);
+    setEditingCenters(null);
+  };
+
     return(
       <div className='Centers-Table'>
         <Row>
@@ -89,20 +93,43 @@ const columns = [
 
         <Row>
           <Col span={10} offset={7}>
-            <Table dataSource={data} columns={columns}></Table>
+            <Table dataSource={dataSource} columns={columns}></Table>
           </Col>
         </Row>
 
         <Form.Item>
         <Row>
-          <Col span={10} offset={7}> 
-            <Button type="primary" className='SaveButton' htmlType="submit" icon={<PlusCircleOutlined  />} onClick={showModal}>Přidat středisko</Button>
-              <Modal title="Středisko" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
-                <p>Some contents...</p>
-              </Modal>
+          <Col span={10} offset={7}>
+            <Button type="primary" className='SaveButton' htmlType="submit" icon={<PlusCircleOutlined  />} onClick={onAddCenter}>Přidat středisko</Button>
           </Col>
         </Row>   
         </Form.Item>
+        <Modal title="Úprava Střediska" visible={isEditing} okText="Uložit" cancelText="Zrušit"
+          onCancel={() => {
+            resetEditing();
+          }}
+          onOk={() => {
+            setDataSource((pre) => {
+              return pre.map((center => {
+                if (center.id === editingCenters.id) {
+                  return editingCenters;
+                } else {
+                  return center;
+                }
+              }));
+            });
+            resetEditing();
+          }}
+        >
+          <Input
+            value={editingCenters?.name}
+            onChange={(e) => {
+              setEditingCenters((pre) => {
+                return { ...pre, center: e.target.value };
+              });
+            }}
+          />
+        </Modal>
       </div>
     );
   }
